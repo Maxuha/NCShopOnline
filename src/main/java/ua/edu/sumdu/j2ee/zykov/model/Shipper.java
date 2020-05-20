50e68a15c5b8075be788219ad09074cc7a5789fa
Maxuha <zykovmaksym@gmail.com>
2020-05-20 12:57:57 +0300

    Added shipper model

--- src/main/java/ua/edu/sumdu/j2ee/zykov/model/Shipper.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/model/Shipper.java
@@ -1,4 +1,50 @@
 package ua.edu.sumdu.j2ee.zykov.model;
 
 public class Shipper {
+    private int id;
+    private String title;
+    private User user;
+    private Order order;
+
+    public Shipper() {
+    }
+
+    public Shipper(int id, String title, User user, Order order) {
+        this.id = id;
+        this.title = title;
+        this.user = user;
+        this.order = order;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
