8cffa18f0a3ac0a528db681d600090ab0d2091d7
Maxuha <zykovmaksym@gmail.com>
2020-05-21 15:35:13 +0300

    Added new request

--- src/main/java/ua/edu/sumdu/j2ee/zykov/controller/CategoryController.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/controller/CategoryController.java
@@ -2,10 +2,26 @@ package ua.edu.sumdu.j2ee.zykov.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ua.edu.sumdu.j2ee.zykov.dao.CategoryDAO;
+import ua.edu.sumdu.j2ee.zykov.model.Category;
+import ua.edu.sumdu.j2ee.zykov.service.CategoryService;
 
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @RequestMapping("/create")
+    public String createCategory() {
+        Category category = new Category(1, "Notebook", "Some description", null);
+        categoryService.create(category);
+        return "categories";
+    }
+
     @RequestMapping()
     public String showAllCategories() {
         return "categories";
