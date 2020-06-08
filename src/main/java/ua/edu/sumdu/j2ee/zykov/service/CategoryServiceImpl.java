73d1665a82cfbde1ad61cf084a5145bb95a1951c
Maxuha <zykovmaksym@gmail.com>
2020-06-08 12:47:20 +0300

    Added CRUD operation

--- src/main/java/ua/edu/sumdu/j2ee/zykov/service/CategoryServiceImpl.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/service/CategoryServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import ua.edu.sumdu.j2ee.zykov.dao.CategoryDAO;
 import ua.edu.sumdu.j2ee.zykov.model.Category;
 
+import java.util.List;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryDAO categoryDAO;
@@ -13,7 +15,22 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
+    public List<Category> getAll() {
+        return categoryDAO.findAll();
+    }
+
+    @Override
     public void create(Category category) {
         categoryDAO.save(category);
+    }
+
+    @Override
+    public void update(Category category) {
+        categoryDAO.update(category);
+    }
+
+    @Override
+    public void delete(Category category) {
+        categoryDAO.delete(category);
     }
 }
