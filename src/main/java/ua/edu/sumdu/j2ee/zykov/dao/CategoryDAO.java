44abda68bd36469f6545614c58d5ea429ea6c99d
Maxuha <zykovmaksym@gmail.com>
2020-06-14 19:19:22 +0300

    Added all CRUD operation

--- src/main/java/ua/edu/sumdu/j2ee/zykov/dao/CategoryDAO.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/dao/CategoryDAO.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface CategoryDAO {
     List<Category> findAll();
+    Category findById(int id);
     void save(Category category);
     void update(Category category);
     void delete(Category category);
--- src/main/java/ua/edu/sumdu/j2ee/zykov/dao/CategoryDAOImpl.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/dao/CategoryDAOImpl.java
@@ -2,6 +2,7 @@ package ua.edu.sumdu.j2ee.zykov.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ua.edu.sumdu.j2ee.zykov.mapper.CategoryMapper;
 import ua.edu.sumdu.j2ee.zykov.model.Category;
 
 import javax.xml.transform.sax.SAXResult;
@@ -16,7 +17,6 @@ import java.util.Optional;
 @Repository
 public class CategoryDAOImpl implements CategoryDAO {
     private final JdbcTemplate jdbcTemplate;
-    List<Category> categories;
 
     public CategoryDAOImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -24,10 +24,44 @@ public class CategoryDAOImpl implements CategoryDAO {
 
     @Override
     public List<Category> findAll() {
-        categories = new ArrayList<>();
-        String query = "SELECT * FROM category";
-
+        String query = "SELECT * FROM category WHERE parent is null";
+        List<Category> mainCategory = jdbcTemplate.query(query, new CategoryMapper());
+        List<Category> categories = new ArrayList<>();
+        for (Category category : mainCategory) {
+            query = "WITH RECURSIVE r AS (" +
+                    "    SELECT id, title, path_to_image, parent, 1 AS level" +
+                    "    FROM category" +
+                    "    WHERE id = ?" +
+                    "    UNION" +
+                    "    SELECT category.id, category.title, category.path_to_image, category.parent, r.level + 1 AS level" +
+                    "    FROM category" +
+                    "             JOIN r" +
+                    "                  ON category.parent = r.id" +
+                    ")" +
+                    "SELECT * FROM r;";
+            categories.addAll(jdbcTemplate.query(query, new CategoryMapper(), category.getId()));
+        }
         return categories;
+    }
+
+    @Override
+    public Category findById(int id) {
+        String query = "WITH RECURSIVE r AS (" +
+                "    SELECT id, title, path_to_image, parent, 1 AS level" +
+                "    FROM category" +
+                "    WHERE id = ?" +
+                "    UNION" +
+                "    SELECT category.id, category.title, category.path_to_image, category.parent, r.level + 1 AS level" +
+                "    FROM category" +
+                "             JOIN r" +
+                "                  ON category.id = r.parent" +
+                ")" +
+                "SELECT * FROM r;";
+        List<Category> categories = jdbcTemplate.query(query, new CategoryMapper(), id);
+        for (int i = 0; i < categories.size() - 1; i++) {
+            categories.get(i).setParent(categories.get(i + 1));
+        }
+        return categories.get(0);
     }
 
     @Override
