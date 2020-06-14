60bbb095beb39ab201d50f33b858fac89272457d
Maxuha <zykovmaksym@gmail.com>
2020-06-14 19:40:57 +0300

    Added full CRUD operation

--- src/main/java/ua/edu/sumdu/j2ee/zykov/dao/CategoryDAOImpl.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/dao/CategoryDAOImpl.java
@@ -5,25 +5,16 @@ import org.springframework.stereotype.Repository;
 import ua.edu.sumdu.j2ee.zykov.mapper.CategoryMapper;
 import ua.edu.sumdu.j2ee.zykov.model.Category;
 
-import javax.xml.transform.sax.SAXResult;
-import org.springframework.stereotype.Repository;
-import ua.edu.sumdu.j2ee.zykov.model.Category;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class CategoryDAOImpl implements CategoryDAO {
-    private final DAOConnection daoConnection;
-    List<Category> categories;
+    private final JdbcTemplate jdbcTemplate;
 
-    public CategoryDAOImpl(DAOConnection daoConnection) {
-        this.daoConnection = daoConnection;
+    public CategoryDAOImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
@@ -44,19 +35,6 @@ public class CategoryDAOImpl implements CategoryDAO {
                     ")" +
                     "SELECT * FROM r;";
             categories.addAll(jdbcTemplate.query(query, new CategoryMapper(), category.getId()));
-        Connection connection = daoConnection.connect();
-        categories = new ArrayList<>();
-        try {
-            daoConnection.setStatement((PreparedStatement) connection.createStatement());
-            daoConnection.setResultSet(daoConnection.getStatement().executeQuery("SELECT * FROM category"));
-            while (daoConnection.getResultSet().next()) {
-                categories.add(parseCategory(daoConnection.getResultSet()));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        finally {
-            daoConnection.disconnect();
         }
         return categories;
     }
@@ -99,62 +77,5 @@ public class CategoryDAOImpl implements CategoryDAO {
     public void delete(Category category) {
         String query = "DELETE FROM category WHERE id = ? ";
         jdbcTemplate.update(query, category.getId());
-    public void create(Category category) {
-        Connection connection = daoConnection.connect();
-        try {
-            daoConnection.setStatement(connection.prepareStatement("INSERT INTO category (title, path_to_image, parent) VALUES (?, ?, ?)"));
-            daoConnection.getStatement().setString(1, category.getTitle());
-            daoConnection.getStatement().setString(2, category.getImageToPath());
-            daoConnection.getStatement().setInt(3, category.getId());
-            daoConnection.getStatement().execute();
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            daoConnection.disconnect();
-        }
-    }
-
-
-    private Category parseCategory(ResultSet resultSet) {
-        Category category = null;
-        try {
-            int id = resultSet.getInt("id");
-            String title = resultSet.getString("title");
-            String pathToImage = resultSet.getString("path_to_image");
-            Integer parentId = resultSet.getInt("parent");
-            category = findCategoryByIdFromList(id);
-            if (parentId != null) {
-                Category parent = findCategoryByIdFromList(parentId);
-                if (parent == null) {
-                    parent = new Category(parentId);
-                    categories.add(parent);
-                }
-                if (category == null) {
-                    category = new Category(id, title, pathToImage, parent);
-                } else {
-                    category.setTitle(title);
-                    category.setImageToPath(pathToImage);
-                    category.setParent(parent);
-                }
-            }
-            if (category == null) {
-                category = new Category(id, title, pathToImage, null);
-            } else {
-                category.setTitle(title);
-                category.setImageToPath(pathToImage);
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return category;
-    }
-
-    private Category findCategoryByIdFromList(int id) {
-        for (Category category : categories) {
-            if (category.getId() == id) {
-                return category;
-            }
-        }
-        return null;
     }
 }
