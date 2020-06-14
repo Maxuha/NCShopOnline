4b36e4f3bb372b970f2aaaa50a810602c3151fc5
Maxuha <zykovmaksym@gmail.com>
2020-06-14 19:23:27 +0300

    Change logic

--- src/main/java/ua/edu/sumdu/j2ee/zykov/mapper/CategoryMapper.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/mapper/CategoryMapper.java
@@ -5,20 +5,29 @@ import ua.edu.sumdu.j2ee.zykov.model.Category;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryMapper implements RowMapper<Category> {
-    Category parent = null;
+    List<Category> categories = new ArrayList<>();
     @Override
     public Category mapRow(ResultSet resultSet, int i) throws SQLException {
         Category category = new Category();
         category.setId(resultSet.getInt("id"));
         category.setTitle(resultSet.getString("title"));
         category.setImageToPath(resultSet.getString("path_to_image"));
-        category.setParent(parent);
         int parentId = resultSet.getInt("parent");
-        if (parent == null || parentId != parent.getId()) {
-            parent = category;
-        }
+        category.setParent(findParentCategoryByIdFromList(parentId));
+        categories.add(category);
         return category;
+    }
+
+    private Category findParentCategoryByIdFromList(int parentId) {
+        for (Category category : categories) {
+            if (category.getId() == parentId) {
+                return category;
+            }
+        }
+        return null;
     }
 }
