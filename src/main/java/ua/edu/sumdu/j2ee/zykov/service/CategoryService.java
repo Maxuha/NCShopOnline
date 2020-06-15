d5e448bf9c7e6678ec234e877727bd5afafe7491
Maxuha <zykovmaksym@gmail.com>
2020-06-15 16:19:44 +0300

    Change name methods

--- src/main/java/ua/edu/sumdu/j2ee/zykov/service/CategoryService.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/service/CategoryService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface CategoryService {
     List<Category> getAll();
     Category getById(int id);
-    Category create(Category category);
-    Category update(Category category);
-    Category delete(Category category);
+    Category addCategory(Category category);
+    Category updateCategory(Category category);
+    Category deleteCategory(Category category);
 }
