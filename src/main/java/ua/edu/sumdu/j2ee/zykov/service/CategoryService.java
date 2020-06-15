669c0e2b8decbd3f265f6cd79f59afcd282c06b7
Maxuha <zykovmaksym@gmail.com>
2020-06-15 11:48:25 +0300

    Change return type

--- src/main/java/ua/edu/sumdu/j2ee/zykov/service/CategoryService.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/service/CategoryService.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface CategoryService {
     List<Category> getAll();
     Category getById(int id);
-    void create(Category category);
-    void update(Category category);
-    void delete(Category category);
+    Category create(Category category);
+    Category update(Category category);
+    Category delete(Category category);
 }
