7d619efa84ea83877beb837bb47ead787918bdf6
Maxuha <zykovmaksym@gmail.com>
2020-06-16 17:24:33 +0300

    Added dao for user

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package ua.edu.sumdu.j2ee.zykov.dao;
+
+
+import ua.edu.sumdu.j2ee.zykov.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    List<User> findAll();
+    User findById(int id);
+    User findByUserName(String username);
+    User save(User user);
+    User update(User user);
+    User delete(User user);
+}
