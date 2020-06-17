398099d18e7bf4b052ab1aa986cc37a09a56580e
Maxuha <zykovmaksym@gmail.com>
2020-06-17 10:57:26 +0300

    Added service layer for shipper

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/service/ShipperServiceImpl.java
@@ -0,0 +1,41 @@
+package ua.edu.sumdu.j2ee.zykov.service;
+
+import org.springframework.stereotype.Service;
+import ua.edu.sumdu.j2ee.zykov.dao.ShipperDAO;
+import ua.edu.sumdu.j2ee.zykov.model.Shipper;
+
+import java.util.List;
+
+@Service
+public class ShipperServiceImpl implements ShipperService {
+    private final ShipperDAO shipperDAO;
+
+    public ShipperServiceImpl(ShipperDAO shipperDAO) {
+        this.shipperDAO = shipperDAO;
+    }
+
+    @Override
+    public List<Shipper> getShipperAll() {
+        return shipperDAO.findAll();
+    }
+
+    @Override
+    public Shipper getShipperById(int id) {
+        return shipperDAO.findById(id);
+    }
+
+    @Override
+    public Shipper addShipper(Shipper shipper) {
+        return shipperDAO.save(shipper);
+    }
+
+    @Override
+    public Shipper updateShipper(Shipper shipper) {
+        return shipperDAO.update(shipper);
+    }
+
+    @Override
+    public Shipper deleteShipper(Shipper shipper) {
+        return shipperDAO.delete(shipper);
+    }
+}
