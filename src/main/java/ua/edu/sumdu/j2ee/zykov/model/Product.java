773ab5ded12772f721db820bdb3e7305dd46f4ad
Maxuha <zykovmaksym@gmail.com>
2020-08-27 12:12:00 +0300

    Added rolback to product

--- src/main/java/ua/edu/sumdu/j2ee/zykov/model/Product.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/model/Product.java
@@ -1,89 +1,24 @@
 package ua.edu.sumdu.j2ee.zykov.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Product {
     private int id;
+    @NonNull
     private String title;
+    @NonNull
     private String description;
+    @NonNull
     private float price;
+    @NonNull
     private float discount;
+    @NonNull
     private Category category;
+    @NonNull
     private Shipper shipper;
-
-    public Product() {
-    }
-
-    public Product(int id, String title, String description, float price, float discount, Category category, Shipper shipper) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.discount = discount;
-        this.category = category;
-        this.shipper = shipper;
-    }
-
-    public Product(String title, String description, float price, float discount, Category category, Shipper shipper) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.discount = discount;
-        this.category = category;
-        this.shipper = shipper;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public float getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(float discount) {
-        this.discount = discount;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Shipper getShipper() {
-        return shipper;
-    }
-
-    public void setShipper(Shipper shipper) {
-        this.shipper = shipper;
-    }
 }
