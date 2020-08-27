dd87856f81678aa44c32756d90c01c398fb689ba
Maxuha <zykovmaksym@gmail.com>
2020-08-27 12:09:54 +0300

    Added rolback to NCUserDetail

--- src/main/java/ua/edu/sumdu/j2ee/zykov/model/NCUserDetail.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/model/NCUserDetail.java
@@ -1,33 +1,18 @@
 package ua.edu.sumdu.j2ee.zykov.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Collection;
 import java.util.Set;
 
+@Getter
+@Setter
 public class NCUserDetail implements UserDetails {
     private static final long serialVersionUID = 1;
     private User user;
     Set<GrantedAuthority> authorities = null;
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-
-    public void setAuthorities(Set<GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
 
     @Override
     public String getPassword() {
