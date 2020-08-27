7f9743b4e89e0874aac8ac7cbde097d0b030fd32
Maxuha <zykovmaksym@gmail.com>
2020-08-27 12:14:26 +0300

    Added rolback to User

--- src/main/java/ua/edu/sumdu/j2ee/zykov/model/User.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/model/User.java
@@ -1,45 +1,16 @@
 package ua.edu.sumdu.j2ee.zykov.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class User {
     private int id;
+    @NonNull
     private String userName;
+    @NonNull
     private String password;
-
-    public User() {
-    }
-
-    public User(String userName, String password) {
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public User(int id, String userName, String password) {
-        this.id = id;
-        this.userName = userName;
-        this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
