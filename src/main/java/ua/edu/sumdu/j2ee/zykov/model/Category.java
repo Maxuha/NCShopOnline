f961d9aac2e0c22bba31f6d0c50840f3b9cc6eae
Maxuha <zykovmaksym@gmail.com>
2020-08-27 11:59:40 +0300

    Added rolback

--- src/main/java/ua/edu/sumdu/j2ee/zykov/model/Category.java
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/model/Category.java
@@ -1,61 +1,18 @@
 package ua.edu.sumdu.j2ee.zykov.model;
 
+import com.sun.istack.internal.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Category {
     private int id;
+    @NotNull
     private String title;
+    @NotNull
     private Image image;
     private Category parent;
-
-    public Category() {
-    }
-
-    public Category(String title, Image image) {
-        this.title = title;
-        this.image = image;
-    }
-
-    public Category(String title, Image image, Category parent) {
-        this.title = title;
-        this.image = image;
-        this.parent = parent;
-    }
-
-    public Category(int id, String title, Image image, Category parent) {
-        this.id = id;
-        this.title = title;
-        this.image = image;
-        this.parent = parent;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Image getImage() {
-        return image;
-    }
-
-    public void setImage(Image image) {
-        this.image = image;
-    }
-
-    public Category getParent() {
-        return parent;
-    }
-
-    public void setParent(Category parent) {
-        this.parent = parent;
-    }
 }
