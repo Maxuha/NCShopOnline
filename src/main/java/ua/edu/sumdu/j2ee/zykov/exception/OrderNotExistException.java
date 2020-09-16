d5430d85ae7b70b2c88f8b356dd6a29762894a77
Maxuha <zykovmaksym@gmail.com>
2020-09-16 14:14:04 +0300

    Added not exist exception for order

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/exception/OrderNotExistException.java
@@ -0,0 +1,7 @@
+package ua.edu.sumdu.j2ee.zykov.exception;
+
+public class OrderNotExistException extends NotExistException {
+    public OrderNotExistException(Long order, Throwable cause) {
+        super(String.format("Order %d not exist", order), cause);
+    }
+}
