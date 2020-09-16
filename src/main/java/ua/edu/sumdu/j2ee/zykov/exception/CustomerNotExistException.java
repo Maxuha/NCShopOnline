fe42c03d7e90182fd69735a35f14dfebccea327e
Maxuha <zykovmaksym@gmail.com>
2020-09-16 14:09:30 +0300

    Added not exist exception for customer

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/exception/CustomerNotExistException.java
@@ -0,0 +1,7 @@
+package ua.edu.sumdu.j2ee.zykov.exception;
+
+public class CustomerNotExistException extends NotExistException {
+    public CustomerNotExistException(Long customerId, Throwable cause) {
+        super(String.format("Customer %d not exist", customerId), cause);
+    }
+}
