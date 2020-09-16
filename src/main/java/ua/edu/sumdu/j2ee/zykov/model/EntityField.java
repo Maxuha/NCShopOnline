2cd3b1353a7c78d6b4a03f82ef0b97f5b8e1a6b2
Maxuha <zykovmaksym@gmail.com>
2020-09-16 17:30:41 +0300

    Added new model for exception

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/model/EntityField.java
@@ -0,0 +1,15 @@
+package ua.edu.sumdu.j2ee.zykov.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntityField {
+    private int id;
+    private String field;
+}
