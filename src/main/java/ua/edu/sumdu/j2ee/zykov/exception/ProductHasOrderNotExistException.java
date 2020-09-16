1d9e6760cad4f369ed25de87a9104f4be4e69046
Maxuha <zykovmaksym@gmail.com>
2020-09-16 14:19:19 +0300

    Added not exist exception for product has order

---
+++ src/main/java/ua/edu/sumdu/j2ee/zykov/exception/ProductHasOrderNotExistException.java
@@ -0,0 +1,7 @@
+package ua.edu.sumdu.j2ee.zykov.exception;
+
+public class ProductHasOrderNotExistException extends NotExistException {
+    public ProductHasOrderNotExistException(Long productHasOrderId, Throwable cause) {
+        super(String.format("Product has order %d not exist", productHasOrderId), cause);
+    }
+}
